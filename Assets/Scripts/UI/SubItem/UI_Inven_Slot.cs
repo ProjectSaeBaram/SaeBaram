63bfd1f7bccf9ead19d32648b5f0f2809324a8c8
BJH <114412598+BJH7536@users.noreply.github.com>
2024-04-15 22:37:47 +0900

    [FIX] 빈 마우스 커서가 아이템 슬롯에서 일으키는 오류 수정
    Co-Authored-By: AerideS <82868023+AerideS@users.noreply.github.com>

--- Assets/Scripts/UI/SubItem/UI_Inven_Slot.cs
+++ Assets/Scripts/UI/SubItem/UI_Inven_Slot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,11 +17,20 @@ public class UI_Inven_Slot : UI_Base, IDropHandler
     public void OnDrop(PointerEventData eventData) {
         // InventorySlot의 자식이 없을 때
         if(transform.childCount == 0) {
-            // eventData가 들고있는 InventoryItem을 받고
-            Item = eventData.pointerDrag.GetComponent<UI_Inven_Item>();
-            // Inventory의 parentAfterDrag를 자신의 transform으로 저장. (복귀 위치가 변경됨.)
-            Item.parentAfterDrag.GetComponent<UI_Inven_Slot>().Item = null;
-            Item.parentAfterDrag = transform;
+
+            try
+            {
+                // eventData가 들고있는 InventoryItem을 받고
+                Item = eventData.pointerDrag.GetComponent<UI_Inven_Item>();
+
+                // Inventory의 parentAfterDrag를 자신의 transform으로 저장. (복귀 위치가 변경됨.)
+                Item.parentAfterDrag.GetComponent<UI_Inven_Slot>().Item = null;
+                Item.parentAfterDrag = transform;
+            }
+            catch (Exception e)
+            {
+                
+            }
         }
     }
 }
