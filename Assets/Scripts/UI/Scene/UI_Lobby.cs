45bace01a88449bbc7f3cb1c9754896a7427607c
BJH <114412598+BJH7536@users.noreply.github.com>
2024-06-15 19:33:51 +0900

    [UPDATE] 로비 UI 추가
    타이틀 및 로고 추가

--- Assets/Scripts/UI/Scene/UI_Lobby.cs
+++ Assets/Scripts/UI/Scene/UI_Lobby.cs
@@ -38,7 +38,7 @@ public class UI_Lobby : UI_Scene
 
     void Settings(PointerEventData data)
     {
-        // Managers.UI.ShowPopupUI<UI_SettingsPopup>();
+        Managers.UI.ShowPopupUI<UI_SettingsPopup>();
     }
 
     public void ExitGame(PointerEventData data)
